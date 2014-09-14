75f089550fdc8d1b4f55128b8a5f80341983e9ab
mfalce <falce.matthieu@gmail.com>
2014-09-14 23:20:10 +0200

    first try with the low power library, I can't mesure de current, there is a leak somewhere with the serial...

--- tests/power_tests/empty_loop.ino
+++ tests/power_tests/empty_loop.ino
@@ -1,7 +1,12 @@
+#include "LowPower.h"
+
 // setup()
 void setup() {
+    Serial.begin(9600); // Initialisation du port série
 }
  
 // loop()
 void loop() {
+  Serial.print("coucou");
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
 }
