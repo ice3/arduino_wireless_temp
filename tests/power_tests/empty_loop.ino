74ddeb85ebd8ff2dff7f3cd91c2f1278571cf0b2
mfalce <falce.matthieu@gmail.com>
2014-09-14 23:43:20 +0200

    used a led to see if the arduino is doing things right

--- tests/power_tests/empty_loop.ino
+++ tests/power_tests/empty_loop.ino
@@ -1,12 +1,19 @@
 #include "LowPower.h"
 
+int led = 13;
+
 // setup()
 void setup() {
+    pinMode(led, OUTPUT);     
     Serial.begin(9600); // Initialisation du port série
 }
  
 // loop()
 void loop() {
   Serial.print("coucou");
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
+  digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
+  LowPower.powerDown(SLEEP_1S, ADC_ON, BOD_ON);  
+  digitalWrite(led, HIGH);    // turn the LED off by making the voltage LOW
+  LowPower.powerDown(SLEEP_1S, ADC_ON, BOD_ON);  
 }
+
