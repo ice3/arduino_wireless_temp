1862c4a6d208dd4ea6ac409884d0b6fe2b1448d7
mfalce <falce.matthieu@gmail.com>
2014-09-14 21:47:44 +0200

    ajout des codes

---
+++ tests/wireless/send/helloworld_tx.ino
@@ -0,0 +1,139 @@
+/*
+ Copyright (C) 2012 James Coliz, Jr. <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+ 
+/** 
+ Envoie de la temperature sans fil 
+**/ 
+
+/**
+ * Simplest possible example of using RF24Network 
+ *
+ * TRANSMITTER NODE
+ * Every 2 seconds, send a payload to the receiver node.
+ */
+
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <OneWire.h> // Inclusion de la librairie OneWire
+ 
+#define DS18B20 0x28     // Adresse 1-Wire du DS18B20
+#define BROCHE_ONEWIRE 7 // Broche utilisée pour le bus 1-Wire
+ 
+OneWire ds(BROCHE_ONEWIRE); // Création de l'objet OneWire ds
+ 
+// Fonction récupérant la température depuis le DS18B20
+// Retourne true si tout va bien, ou false en cas d'erreur
+boolean getTemperature(float *temp){
+  byte data[9], addr[8];
+  // data : Données lues depuis le scratchpad
+  // addr : adresse du module 1-Wire détecté
+ 
+ Serial.print("search\n");
+  if (!ds.search(addr)) { // Recherche un module 1-Wire
+    ds.reset_search();    // Réinitialise la recherche de module
+    return false;         // Retourne une erreur
+  }
+  Serial.print("oneWire\n"); 
+  if (OneWire::crc8(addr, 7) != addr[7]) // Vérifie que l'adresse a été correctement reçue
+    return false;                        // Si le message est cor/rompu on retourne une erreur
+ 
+  if (addr[0] != DS18B20) // Vérifie qu'il s'agit bien d'un DS18B20
+    return false;         // Si ce n'est pas le cas on retourne une erreur
+ 
+  ds.reset();             // On reset le bus 1-Wire
+  ds.select(addr);        // On sélectionne le DS18B20
+   
+  ds.write(0x44, 1);      // On lance une prise de mesure de température
+  delay(800);             // Et on attend la fin de la mesure
+   
+  ds.reset();             // On reset le bus 1-Wire
+  ds.select(addr);        // On sélectionne le DS18B20
+  ds.write(0xBE);         // On envoie une demande de lecture du scratchpad
+ 
+  for (byte i = 0; i < 9; i++) // On lit le scratchpad
+    data[i] = ds.read();       // Et on stock les octets reçus
+   
+  // Calcul de la température en degré Celsius
+  *temp = ((data[1] << 8) | data[0]) * 0.0625;
+   
+  // Pas d'erreur
+  return true;
+}
+
+
+// nRF24L01(+) radio attached using Getting Started board 
+RF24 radio(9,10);
+
+// Network uses that radio
+RF24Network network(radio);
+
+// Address of our node
+const uint16_t this_node = 1;
+
+// Address of the other node
+const uint16_t other_node = 0;
+
+// How often to send 'hello world to the other unit
+const unsigned long interval = 10000; //ms
+
+// When did we last send?
+unsigned long last_sent;
+
+// How many have we sent already
+unsigned long packets_sent;
+
+// Structure of our payload
+struct payload_t
+{
+  unsigned long ms;
+  unsigned long counter;
+};
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println("RF24Network/examples/helloworld_tx/");
+ 
+  SPI.begin();
+  radio.begin();
+  network.begin(/*channel*/ 90, /*node address*/ this_node);
+}
+
+void loop(void)
+{
+    float temp = -1;
+  // Lit la température ambiante à ~1Hz
+  if(getTemperature(&temp)) {
+     
+    // Affiche la température
+    Serial.print("Temperature : ");
+    Serial.print(temp);
+    Serial.write(176); // caractère °
+    Serial.write('C');
+    Serial.println();
+  }
+  
+  // Pump the network regularly
+  network.update();
+
+  // If it's time to send a message, send it!
+  unsigned long now = millis();
+  if ( now - last_sent >= interval  )
+  {
+    last_sent = now;
+    payload_t payload = { temp*100, packets_sent++ };
+    RF24NetworkHeader header(/*to node*/ other_node);
+    bool ok = network.write(header,&payload,sizeof(payload));
+    if (ok)
+      Serial.println("ok.");
+    else
+      Serial.println("failed.");
+  }
+}
+// vim:ai:cin:sts=2 sw=2 ft=cpp
