c8127f0e1bd5994643b4d2fb5a4d387e9385f56b
mfalce <falce.matthieu@gmail.com>
2014-09-15 22:41:33 +0200

    after some struggling with the schematic, I can have a 0.02mA consumation in power off mode

--- tests/power_tests/empty_loop.ino
+++ tests/power_tests/empty_loop.ino
@@ -10,10 +10,10 @@ void setup() {
  
 // loop()
 void loop() {
-  Serial.print("coucou");
-  digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
-  LowPower.powerDown(SLEEP_1S, ADC_ON, BOD_ON);  
-  digitalWrite(led, HIGH);    // turn the LED off by making the voltage LOW
-  LowPower.powerDown(SLEEP_1S, ADC_ON, BOD_ON);  
+  // Serial.print("coucou");
+  // digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
+  // digitalWrite(led, HIGH);    // turn the LED off by making the voltage LOW
+  // LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);  
 }
 
